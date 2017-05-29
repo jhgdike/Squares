29fc9cc57630fed974ac803d0a56e69e4a0108f3
王涛 <wangtao101rs@163.com>
2017-05-29 14:44:08 +0800

    fix schema

--- squares/views/schema/table.py
+++ squares/views/schema/table.py
@@ -4,9 +4,10 @@ from marshmallow import fields, Schema
 class TableSchema(Schema):
     """table schema"""
     table_id = fields.String()
-    players = fields.List(fields.Integer())
+    players = fields.List(fields.String())
     turn = fields.Integer(default=0, attribute='player_n')
     is_owner = fields.Boolean()
     is_started = fields.Boolean()
+    squares = fields.List(fields.List(fields.Integer()))
 
 table_schema = TableSchema()
